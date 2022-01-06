0f2f1924af5b7b941856016d711883b1e8e77bf6
alish <aldc1233@gmail.com>
2022-01-06 11:10:57 +0545

    first commit

---
+++ src/main/java/com/practice/spring/datatabless/DatatablessApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.datatabless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatablessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatatablessApplication.class, args);
+	}
+
+}
